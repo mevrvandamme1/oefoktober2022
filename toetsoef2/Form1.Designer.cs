14d7f768f32016ac0ebc114180a3ca3d5d99e99b
Maggie Van Damme <maggie.van.damme1970@outlook.com>
2022-10-25 09:47:29 +0200

    versie 3

--- toetsoef2/Form1.Designer.cs
+++ toetsoef2/Form1.Designer.cs
@@ -46,8 +46,10 @@ namespace toetsoef2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
