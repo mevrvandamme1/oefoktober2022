6bb623791e78ef30515a6a8827ba0ef911f08df6
Maggie Van Damme <maggie.van.damme1970@outlook.com>
2022-10-25 10:05:19 +0200

    versie 5

--- toetsoef2/Form1.Designer.cs
+++ toetsoef2/Form1.Designer.cs
@@ -34,12 +34,13 @@ namespace toetsoef2
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button1.Location = new System.Drawing.Point(155, 121);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(343, 106);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
