22bf102dae9850c04b911e554a439deceb1512f2
Maggie Van Damme <maggie.van.damme1970@outlook.com>
2022-10-25 10:23:50 +0200

    versie11

--- toetsoef2/Form1.Designer.cs
+++ toetsoef2/Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace toetsoef2
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(343, 106);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Naam:";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
